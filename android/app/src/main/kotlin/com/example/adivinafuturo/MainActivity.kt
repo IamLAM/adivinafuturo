ecbeb926d129fddd134a5c818f5da8b5f9ceb8c1
Luis Angel Monge <ingeniero@luismonge.com.mx>
2024-06-21 16:41:05 -0700

    Base del miniproyecto adivina el futuro

---
+++ android/app/src/main/kotlin/com/example/adivinafuturo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.adivinafuturo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "Preguntame",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
